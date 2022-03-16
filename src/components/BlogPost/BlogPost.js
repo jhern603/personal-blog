feeb9773b5dba9e4f9682d379ccff02908be9cf7
jhern603 <jhern603@gmail.com>
2022-03-16 17:29:43 -0400

    initial commit

---
+++ src/components/BlogPost/BlogPost.js
@@ -0,0 +1,16 @@
+export const Post = () => {
+  const posts = require('../../posts.json');
+  return (
+    <div className="post__container">
+      {posts.map((post) => {
+        return (
+          <div>
+            <h1 className="post__header">{post.title}</h1>
+            <h3 className="post__author">By {post.author}</h3>
+            <span className="post__content">{post.content}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
